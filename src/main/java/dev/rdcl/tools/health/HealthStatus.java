370ee2c504b5443d77e27f3c885734cb691835b0
Ruud van der Weide <rj@rdcl.dev>
2024-12-01 13:34:38 +0100

    Upgrade to java 23

--- src/main/java/dev/rdcl/tools/health/HealthStatus.java
+++ src/main/java/dev/rdcl/tools/health/HealthStatus.java
@@ -4,46 +4,30 @@ import java.util.Optional;
 
 public class HealthStatus {
 
-    /**
-     * If healthy, counts the number of failures. If unhealthy, counts the number of successes.
-     */
+    /// If healthy, counts the number of failures. If unhealthy, counts the number of successes.
     private int count = 0;
 
-    /**
-     * Whether the system is healthy or unhealthy.
-     */
+    /// Whether the system is healthy or unhealthy.
     private boolean healthy = true;
 
-    /**
-     * If true, indicates that the health status has changed since the last time it was checked.
-     */
+    /// If true, indicates that the health status has changed since the last time it was checked.
     private boolean statusHasChanged = false;
 
-    /**
-     * The number of consecutive successes needed for the service to be considered to be healthy.
-     */
+    /// The number of consecutive successes needed for the service to be considered to be healthy.
     private final int healthyThreshold;
 
-    /**
-     * The number of consecutive failures needed for the service to be considered to be unhealthy.
-     */
+    /// The number of consecutive failures needed for the service to be considered to be unhealthy.
     private final int unhealthyThreshold;
 
-    /**
-     * @param healthyThreshold   The number of consecutive successes needed for the service to be considered to be
-     *                           healthy.
-     * @param unhealthyThreshold The number of consecutive failures needed for the service to be considered to be
-     *                           unhealthy.
-     */
+    /// @param healthyThreshold   The number of consecutive successes needed for the service to be considered healthy.
+    /// @param unhealthyThreshold The number of consecutive failures needed for the service to be considered unhealthy.
     public HealthStatus(int healthyThreshold, int unhealthyThreshold) {
         this.healthyThreshold = healthyThreshold;
         this.unhealthyThreshold = unhealthyThreshold;
     }
 
-    /**
-     * Register a success. If healthy, this will reset the failure count. If unhealthy, this will count towards the
-     * success count.
-     */
+    /// Register a success. If healthy, this will reset the failure count. If unhealthy, this will count towards the
+    /// success count.
     public synchronized void registerSuccess() {
         if (healthy) {
             count = 0;
@@ -57,10 +41,8 @@ public class HealthStatus {
         }
     }
 
-    /**
-     * Register a failure. If healthy, this will count towards the failure count. If unhealthy, this will reset the
-     * success count.
-     */
+    /// Register a failure. If healthy, this will count towards the failure count. If unhealthy, this will reset the
+    /// success count.
     public synchronized void registerFailure() {
         if (healthy) {
             count += 1;
@@ -74,12 +56,10 @@ public class HealthStatus {
         }
     }
 
-    /**
-     * Checks if the health status has changed since the previous check.
-     *
-     * @return An optional containing a boolean value representing the health status if the health status has changed
-     * since the previous check. An empty optional if the status did not change.
-     */
+    /// Checks if the health status has changed since the previous check.
+    ///
+    /// @return An optional containing a boolean value representing the health status if the health status has changed
+    /// since the previous check. An empty optional if the status did not change.
     public synchronized Optional<Boolean> checkIfChanged() {
         if (statusHasChanged) {
             statusHasChanged = false;
